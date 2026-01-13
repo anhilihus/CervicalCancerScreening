f945a6f042ae4ff6be1a09ccff382c970723891a
Nikhil Yadav <92751331+anhilihus@users.noreply.github.com>
2026-01-13 15:11:40 +0530

    Add files via upload

--- compress_model.py
+++ compress_model.py
@@ -6,7 +6,7 @@ model = joblib.load('model.pkl')
 print(f"Original size: {os.path.getsize('model.pkl')/1024/1024:.2f} MB")
 
 print("Compressing...")
-joblib.dump(model, 'model.pkl', compress=9)
+joblib.dump(model, 'model.pkl', compress=9, protocol=4)
 
 print("Done.")
 print(f"New size: {os.path.getsize('model.pkl')/1024/1024:.2f} MB")
